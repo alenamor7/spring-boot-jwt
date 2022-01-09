c0ba827285f0604b65e9c9b3c153bb27e12196d7
alenamor7 <morozkinalyona@gmail.com>
2022-01-09 15:47:24 +0100

    feat: initialize project, create postgres conn, entities, repositories

---
+++ src/main/java/com/example/springbootjwt/models/ERole.java
@@ -0,0 +1,7 @@
+package com.example.springbootjwt.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/example/springbootjwt/service/UserDetailsImpl.java
@@ -0,0 +1,55 @@
+package com.example.springbootjwt.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String userName;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
